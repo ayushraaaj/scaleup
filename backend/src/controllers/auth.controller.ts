2c92f72be21b53e907945b0e960d4720088afa31
Ayush Raj <ayushrajar000@gmail.com>
2026-09-07 23:56:12 +0530

    minor change

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -145,7 +145,7 @@ export const refreshToken = asyncHandler(
       httpOnly: true,
       secure: true,
       sameSite: "none" as const,
-      path: "/api/v1/auth/refresh-token",
+      path: "/api/v1/refresh-token",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     };
 
