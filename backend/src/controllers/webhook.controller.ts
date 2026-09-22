6772b5bf6bbe0b2b4c4d9b59eeb41c154501782e
Ayush Raj <ayushrajar000@gmail.com>
2026-09-22 22:26:39 +0530

    minor change

--- backend/src/controllers/webhook.controller.ts
+++ backend/src/controllers/webhook.controller.ts
@@ -7,6 +7,8 @@ export const handleEmailWebhook = asyncHandler(
   async (req: Request, res: Response) => {
     const { id, event, "message-id": providerMessageId, ts_event } = req.body;
 
+    console.log("Webhook Received: ", req.body);
+
     await EmailWebHookEvent.create({
       webhookId: id,
       providerMessageId,
